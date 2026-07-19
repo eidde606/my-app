67ed3f5d1320b249e622f5b5f5c5f939fb7e8657
eidde606 <eidde_606@yahoo.com>
2026-07-18 20:07:32 -0400

    Connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
